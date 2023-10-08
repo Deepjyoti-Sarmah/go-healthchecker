03775dd3257941546dc8871f92901a0897a0214e
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-10-08 21:21:15 +0530

    creating a website health checker

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Deepjyoti-Sarmah/go-healthchecker
+
+go 1.20
+
+require github.com/urfave/cli/v2
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import(
+  "github.com/urfave/cli/v2"
+)
+
+func main()  {
+  app := &cli.App{
+    Name:"healthchecker",
+    Usage:"A tiny tool that checks wheather a website is running or is down",
+    Flags:[]cli.Flag{
+      &cli.StringFlag{
+        Name: "domain",
+        Aliases: []string{"d"},
+        Usage: "Domain name to check",
+        Require: true,
+      },
+      &cli.StringFlag{
+        Name: "port",
+        Aliases: []string{"p"},
+        Usage: "Port number to check.",
+        Require: false,
+      },
+    },
+    Action: func(c *cli.Context) error {
+      port := c.String("port")
+      if c.String("port") == "" {
+         port = "80"
+      }
+      status := Check(c.String("domain"), port)
+      fmt.Println(status)
+      return nil
+    },
+  }
+  err := app.Run(os.Args)
+  if err != nil {
+    log.Fatal(err);
+  }
+}
